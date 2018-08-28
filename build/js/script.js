e117e9c46acd537b797069b6fd838ca25ddcdbaf
nhomerc <39440448+nhomerc@users.noreply.github.com>
2018-08-28 21:58:44 +0300

    уменьшено меню, добавлена кнопка скрытия меню

--- build/js/script.js
+++
@@ -1,1 +0,0 @@
-var showHideButton=document.querySelector(".nav__button"),mainMenu=document.querySelector(".nav__list");showHideButton.addEventListener("click",function(e){e.preventDefault(),mainMenu.classList.toggle("show")}),showHideButton.addEventListener("click",function(e){e.preventDefault(),showHideButton.classList.toggle("open")});
--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,12 @@
 var showHideButton = document.querySelector(".nav__button");
 var mainMenu = document.querySelector(".nav__list");
-// mainMenu.classList.toggle("show");
 showHideButton.addEventListener("click", function(evt) {
     evt.preventDefault();
     mainMenu.classList.toggle("show");
 })
-// showHideButton.classList.toggle("open");
-showHideButton.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  showHideButton.classList.toggle("open");
+
+document.addEventListener("keydown", function(e) {
+  e.preventDefault();
+  if(e.keyCode == 27) mainMenu.classList.remove("show");
 })
+
