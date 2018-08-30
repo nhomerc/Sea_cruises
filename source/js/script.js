675e8a3c4e5435e01e5c2a8673c0ee261c0bf4d6
nhomerc <39440448+nhomerc@users.noreply.github.com>
2018-08-30 21:28:34 +0300

    выполнена версия для планшета

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,9 @@
 var showHideButton = document.querySelector(".nav__button");
 var mainMenu = document.querySelector(".nav__list");
+var showPopUpButton = document.querySelector(".catalog__button");
+var popUp = document.querySelector(".catalog-popup");
+var closePopUp = document.querySelector(".catalog-popup__button");
+var layer = document.querySelector(".overlay");
 showHideButton.addEventListener("click", function(evt) {
     evt.preventDefault();
     mainMenu.classList.toggle("show");
@@ -8,5 +12,14 @@ showHideButton.addEventListener("click", function(evt) {
 document.addEventListener("keydown", function(e) {
   e.preventDefault();
   if(e.keyCode == 27) mainMenu.classList.remove("show");
+})
+showPopUpButton.addEventListener("click", function(pop) {
+  pop.preventDefault();
+  popUp.classList.add("show");
+  layer.style.display = "block";
+})
+closePopUp.addEventListener("click", function(close) {
+  popUp.classList.remove("show");
+  layer.style.display = "none";
 })
 
